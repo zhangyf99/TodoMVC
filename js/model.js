f34b0cc1b8c78d8e270971823a36a26c1e4c07ed
yfzhang <542282472@qq.com>
2020-06-26 23:32:03 +0800

    finish

---
+++ js/model.js
@@ -0,0 +1,15 @@
+window.model = {
+  data: {
+    items: [
+      // {msg:'', date:'',completed: false}
+    ],
+    msg: '',
+    date:'',
+    filter: 'All'
+  },
+  TOKEN: 'Todo'
+  
+  // data provider interface
+  // init: null
+  // flush: null
+};
